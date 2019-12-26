a9e999e5e121319af0e3ba26915287cd6aae04a7
zhuyiling <yiling.zhu@realai.ai>
2019-12-26 11:07:35 +0800

    first

---
+++ train.py
@@ -0,0 +1,85 @@
+import os
+import torch
+from dataset import return_MVTecAD_loader
+from network import VAE,loss_function
+import matplotlib.pyplot as plt
+
+def train(model,train_loader,device,optimizer,epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, data in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        recon_batch = model(data)
+        loss = loss_function(recon_batch, data, model.mu, model.logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+    train_loss /= len(train_loader.dataset)
+
+
+def eval(model,test_loader,device):
+    model.eval()
+    x_0 = iter(test_loader).next()
+    with torch.no_grad():
+        x_vae = model(x_0.to(device)).detach().cpu().numpy()
+
+
+        
+# gif作成用
+def iterative_plot(x_t, j):
+    plt.figure(figsize=(15, 4))
+    for i in range(10):
+        plt.subplot(1, 10, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(x_t[i][0], cmap=plt.cm.gray)
+    plt.subplots_adjust(wspace=0., hspace=0.)        
+    plt.savefig("./results/{}.png".format(j))
+    plt.show()
+    
+def main():
+    train_loader = return_MVTecAD_loader("./mvtec_anomaly_detection/grid/train/good/")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    seed = 42
+    out_dir = './logs'
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        
+    model = VAE(z_dim=512).to(device)
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
+    num_epochs = 500
+    for epoch in range(num_epochs):
+        loss = train(model=model,train_loader=train_loader,device=device,optimizer=optimizer,epoch=epoch)
+        print('epoch [{}/{}], train loss: {:.4f}'.format(epoch + 1,num_epochs,loss.item()))
+    
+    test_loader = return_MVTecAD_loader("./mvtec_anomaly_detection/grid/test/metal_contamination/", batch_size=10, train=False)    
+    eval(model=model,test_loader=test_loader)
+    
+    alpha = 0.05
+    lamda = 1
+
+    x_0 = x_0.to(device).clone().detach().requires_grad_(True)
+    recon_x = model(x_0).detach()
+    loss = F.binary_cross_entropy(x_0, recon_x, reduction='sum')  
+    loss.backward(retain_graph=True)
+
+    x_grad = x_0.grad.data
+    x_t = x_0 - alpha * x_grad * (x_0 - recon_x) ** 2
+
+    for i in range(15):
+        recon_x = model(x_t).detach()
+        loss = F.binary_cross_entropy(x_t, recon_x, reduction='sum') + lamda * torch.abs(x_t - x_0).sum()
+        loss.backward(retain_graph=True)
+
+        x_grad = x_0.grad.data
+        x_t = x_t - eps * x_grad * (x_t - recon_x) ** 2
+        iterative_plot(x_t.detach().cpu().numpy(), i)
