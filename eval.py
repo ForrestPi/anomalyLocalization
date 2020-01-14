9df5690510b65498701032fcc46b67206e2654ab
zhuyiling <yiling.zhu@realai.ai>
2020-01-14 16:35:50 +0800

    paper01

--- eval.py
+++ eval.py
@@ -5,21 +5,6 @@ from dataset import return_MVTecAD_loader
 from network import VAE,loss_function
 import matplotlib.pyplot as plt
 
-def train(model,train_loader,device,optimizer,epoch):
-    model.train()
-    train_loss = 0
-    for batch_idx, data in enumerate(train_loader):
-        data = data.to(device)
-        optimizer.zero_grad()
-        recon_batch = model(data)
-        loss = loss_function(recon_batch, data, model.mu, model.logvar)
-        loss.backward()
-        train_loss += loss.item()
-        optimizer.step()
-    train_loss /= len(train_loader.dataset)
-    return train_loss
-
-
 def eval(model,test_loader,device):
     model.eval()
     x_0 = iter(test_loader).next()
@@ -38,6 +23,7 @@ def EBM(model,test_loader,device):
     loss.backward(retain_graph=True)
 
     x_grad = x_0.grad.data
+    
     x_t = x_0 - alpha * x_grad * (x_0 - recon_x) ** 2
 
     for i in range(15):
@@ -46,7 +32,9 @@ def EBM(model,test_loader,device):
         loss.backward(retain_graph=True)
 
         x_grad = x_0.grad.data
-        eps = 0.001
+        #eps = 0.028
+        x_grad = F.normalize(x_grad)
+        eps = 0.4
         x_t = x_t - eps * x_grad * (x_t - recon_x) ** 2
         iterative_plot(x_t.detach().cpu().numpy(), i)
 
@@ -64,8 +52,6 @@ def iterative_plot(x_t, j):
     #plt.show()
     
 def main():
-    train_loader = return_MVTecAD_loader(image_dir="./mvtec_anomaly_detection/grid/train/good/", batch_size=256, train=True)
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
@@ -81,17 +67,14 @@ def main():
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         
-    model = VAE(z_dim=512).to(device)
+    model = VAE(z_dim=512)
+    model.load_state_dict(torch.load("./checkpoints/500.pth"))
+    model=model.to(device)
     
     optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
-    num_epochs = 500
-    for epoch in range(num_epochs):
-        loss = train(model=model,train_loader=train_loader,device=device,optimizer=optimizer,epoch=epoch)
-        print('epoch [{}/{}], train loss: {:.4f}'.format(epoch + 1,num_epochs,loss))
-        if (epoch+1) % 10 == 0:
-            torch.save(model.state_dict(), os.path.join(checkpoints_dir,"{}.pth".format(epoch+1)))
+    
     test_loader = return_MVTecAD_loader(image_dir="./mvtec_anomaly_detection/grid/test/metal_contamination/", batch_size=10, train=False)    
-    eval(model=model,test_loader=test_loader,device=device)
+    #eval(model=model,test_loader=test_loader,device=device)
     EBM(model,test_loader,device)
     
 if __name__ == "__main__":
